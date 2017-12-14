8aadb48b95d83f65a7f1cfd451674d87f3bbaa22
sanjib007 <loton1984@gmail.com>
2017-12-15 00:11:53 +0600

    this is 2nd commit

--- app/User.php
+++ app/User.php
@@ -26,4 +26,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function role(){
+        return $this->belongsTo(Role::class);
+    }
+
 }
